80f2ca98d6098b019ee72db16b6b50dabf5eeef4
masterfedservices <45753647+masterfedservices@users.noreply.github.com>
2025-02-15 19:55:42 -0600

    Removed unused components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,22 +92,6 @@ const config: Config = {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		keyframes: {
-  			marquee: {
-  				from: {
-  					transform: 'translateX(0)'
-  				},
-  				to: {
-  					transform: 'translateX(calc(-100% - var(--gap)))'
-  				}
-  			},
-  			'marquee-vertical': {
-  				from: {
-  					transform: 'translateY(0)'
-  				},
-  				to: {
-  					transform: 'translateY(calc(-100% - var(--gap)))'
-  				}
-  			},
   			orbit: {
   				'0%': {
   					transform: 'rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)'
@@ -252,8 +236,6 @@ const config: Config = {
   			}
   		},
   		animation: {
-  			marquee: 'marquee var(--duration) infinite linear',
-  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
   			orbit: 'orbit calc(var(--duration)*1s) linear infinite',
   			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
   			shine: 'shine var(--duration) infinite linear',
